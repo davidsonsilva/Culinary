ce6c3532e13309b552074b9eb9fd72f89ef9a58e
davidsonsilva <davidson.f.silva@gmail.com>
2018-08-29 06:13:08 -0300

    Novas imagens e view pager incluidos

---
+++ app/src/main/java/silva/davidson/com/br/culinary/adapter/RecipeRecycleViewAdapter.java
@@ -0,0 +1,89 @@
+package silva.davidson.com.br.culinary.adapter;
+
+import android.app.Application;
+import android.databinding.DataBindingUtil;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import java.util.List;
+
+import silva.davidson.com.br.culinary.R;
+import silva.davidson.com.br.culinary.databinding.ItemRecipeBinding;
+import silva.davidson.com.br.culinary.db.CulinaryDataBase;
+import silva.davidson.com.br.culinary.model.Recipe;
+import silva.davidson.com.br.culinary.viewModel.RecipeViewModel;
+
+public class RecipeRecycleViewAdapter extends RecyclerView.Adapter<RecipeRecycleViewAdapter.ViewHolder> {
+
+    private List<Recipe> recipeArrayList;
+    private Application mContext;
+
+    public RecipeRecycleViewAdapter(Application context){
+        mContext = context;
+    }
+
+    public void setRecipes(List<Recipe> recipes) {
+        this.recipeArrayList = recipes;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public RecipeRecycleViewAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        ItemRecipeBinding mRecipeBinding = DataBindingUtil.inflate(
+                LayoutInflater.from(viewGroup.getContext()),
+                R.layout.item_recipe, viewGroup, false);
+        return new ViewHolder(mRecipeBinding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecipeRecycleViewAdapter.ViewHolder viewHolder, int position) {
+        viewHolder.binding(recipeArrayList.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return recipeArrayList != null ? recipeArrayList.size() : 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        private ItemRecipeBinding recipeBinding;
+        private RecipeViewModel viewModel;
+
+        public ViewHolder(@NonNull ItemRecipeBinding itemRecipeBinding) {
+            super(itemRecipeBinding.getRoot());
+            recipeBinding = itemRecipeBinding;
+        }
+
+        public void binding(Recipe recipe){
+            viewModel = new RecipeViewModel(mContext, CulinaryDataBase.getInstance(mContext));
+            viewModel.getRecipe().setValue(recipe);
+            recipeBinding.setViewModel(viewModel);
+            recipeBinding.avatarImage.setImageResource(viewModel.setRoundIcon(recipe.getName()));
+            setImageResource(recipe.getName(), recipeBinding.mediaImage);
+        }
+
+        private void setImageResource (String recipeValue, ImageView imageView) {
+            switch (recipeValue) {
+                case "Nutella Pie":
+                    imageView.setImageResource(R.mipmap.ic_nutela_pie_foreground);
+                    break;
+                case "Brownies":
+                    imageView.setImageResource(R.mipmap.ic_brownies_foreground);
+                    break;
+                case "Yellow Cake":
+                    imageView.setImageResource(R.mipmap.ic_yellow_cake_foreground);
+                    break;
+                case "Cheesecake":
+                    imageView.setImageResource(R.mipmap.ic_cheesecake_foreground);
+                    break;
+                default : imageView.setImageResource(R.mipmap.ic_cheesecake_foreground);
+                    break;
+            }
+        }
+    }
+}
