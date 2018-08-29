d616a4f362330ad555e003bbeb8b816cc43a6b6f
davidsonsilva <davidson.f.silva@gmail.com>
2018-08-28 21:17:41 -0300

    Incluindo DataBinding na Main Activity

--- app/src/main/java/silva/davidson/com/br/culinary/MainActivity.java
+++ app/src/main/java/silva/davidson/com/br/culinary/MainActivity.java
@@ -2,37 +2,46 @@ package silva.davidson.com.br.culinary;
 
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 
 import java.util.ArrayList;
 
 import silva.davidson.com.br.culinary.adapter.RecipeRecycleViewAdapter;
+import silva.davidson.com.br.culinary.databinding.ActivityMainBinding;
 import silva.davidson.com.br.culinary.factory.ViewModelFactory;
 import silva.davidson.com.br.culinary.model.Recipe;
 import silva.davidson.com.br.culinary.viewModel.RecipeViewModel;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding mMainBinding;
     private RecipeRecycleViewAdapter adapter;
     private RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        setSupportActionBar(mMainBinding.mainToolbar);
+        mMainBinding.mainToolbar.setLogo(R.mipmap.ic_culinary);
+        mMainBinding.mainToolbar.setTitleTextColor(getResources().getColor(R.color.text_primary));
+
         ViewModelFactory factory = ViewModelFactory.getInstance(getApplication());
         RecipeViewModel viewModel = ViewModelProviders.of(this, factory).get(RecipeViewModel.class);
-        mRecyclerView = findViewById(R.id.recipe_list);
+
         viewModel.loadRecipes();
+
         viewModel.getRecipeMutableLiveData().observe(this, new Observer<ArrayList<Recipe>>() {
             @Override
             public void onChanged(@Nullable ArrayList<Recipe> recipes) {
                 adapter = new RecipeRecycleViewAdapter(getApplication());
                 adapter.setRecipes(recipes);
-                mRecyclerView.setAdapter(adapter);
+                mMainBinding.recipeList.setAdapter(adapter);
             }
         });
     }
